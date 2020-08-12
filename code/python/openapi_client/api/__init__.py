303f59d0812b838794de0a4aad68ccc02f5b29c7
fsamwel <frank.samwel@vng.nl>
2020-08-12 10:47:28 +0200

    Toevoegen client code in Python

---
+++ code/python/openapi_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.adres_api import AdresApi
+from openapi_client.api.adresseerbaar_object_api import AdresseerbaarObjectApi
+from openapi_client.api.pand_api import PandApi
