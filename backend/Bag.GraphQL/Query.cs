90268db6249e463da28c10d74786e1c0269b67db
Melvin Lee <melvin.lee@iswish.nl>
2021-06-07 09:07:14 +0200

    feat: initial setup backend

---
+++ backend/Bag.GraphQL/Query.cs
@@ -0,0 +1,22 @@
+﻿using Bag.GraphQL.GraphQL;
+using HotChocolate.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bag.GraphQL
+{
+    public class Query
+    {
+        [UseFiltering]
+        public IQueryable<AdresseerbaarObject> GetAdresseerbareObjecten()
+        {
+            return new List<AdresseerbaarObject>().AsQueryable();
+        }
+
+        [UseFiltering]
+        public IQueryable<Pand> Panden()
+        {
+            return new List<Pand>().AsQueryable();
+        }
+    }
+}
